195300091c40284dbabdb95d80e15fbaffadbfcc
Franz Pucher <franz.pucher@gmail.com>
2017-11-20 21:16:46 +0100

    basic pid implementation

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,4 +1,5 @@
 #include "PID.h"
+#include <iostream>
 
 using namespace std;
 
@@ -11,11 +12,25 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+  PID::Kp = Kp;
+  PID::Ki = Ki;
+  PID::Kd = Kd;
+
+  p_error = 0.0;
+  i_error = 0.0;
+  d_error = 0.0;
 }
 
 void PID::UpdateError(double cte) {
+  p_error = cte;
+  //cout << "p_error" << p_error << endl;
+  i_error += cte;
+  //cout << "i_error" << i_error << endl;
+  d_error = cte - p_error;
+  //cout << "d_error" << d_error << endl;
 }
 
 double PID::TotalError() {
+  double total_error = -Kp * p_error - Kd * d_error - Ki * i_error;
+  return total_error;
 }
-
